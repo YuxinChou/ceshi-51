9fadf741d770bb8bac8e8cc3e85d7622fdc2acda
yuxin0721 <yuxin0721@gmail.com>
2023-05-03 18:44:23 +0800

    feat(image): add lazyload for image

--- js/data.js
+++ js/data.js
@@ -1,23 +1,32 @@
 var data = {
     '1': {
         name: 'famo_03',
+        category: '',
+        author: '',
         type: 'normal',
         totalPics: 5,
     },
     '001': {
         name: 'wuhuaguo_01',
+        category: '',
+        author: '',
         type: 'video',
         totalPics: 0,
     },
     '2': {
         name: 'wuhuaguo_04',
+        category: '',
+        author: '',
         type: 'normal',
         totalPics: 7,
     },
     '002': {
         name: 'lengcuichayou_02',
+        category: '',
+        author: '',
         type: 'video',
         totalPics: 0,
+
     }
 }
 
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,4 @@
+// 获取url内参数
 function queryURLParams(url) {
     let pattern = /(\w+)=(\w+)/ig; //定义正则表达式
     let params = {}; // 定义参数对象
@@ -5,4 +6,27 @@ function queryURLParams(url) {
         params[$1] = $2;
     });
     return params;
+}
+
+// 图片懒加载
+function lazyLoad(imgs) {
+    var H = document.documentElement.clientHeight;//获取可视区域高度
+    var S = document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(`H: ${H}, S: ${S}, H+S: ${H+S}, `);
+    for (var i = 0; i < imgs.length; i++) {
+        console.log(`i: ${i}, getTop(imgs[i]): ${getTop(imgs[i])}`);
+        if (H + S > getTop(imgs[i])) {
+            imgs[i].src = imgs[i].getAttribute('data-src');
+        }
+    }
+}
+
+// 图片懒加载的判断
+// offsetTop是元素与offsetParent的距离，循环获取直到页面顶部
+function getTop(e) {
+    var T = e.offsetTop;
+    while(e = e.offsetParent) {
+        T += e.offsetTop;
+    }
+    return T;
 }
