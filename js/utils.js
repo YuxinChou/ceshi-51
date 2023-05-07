8170ad9a5d3459a93ae598c35bf19bd119d797f2
yuxin0721 <yuxin0721@gmail.com>
2023-05-07 17:45:40 +0800

    feat(code): update

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 // 获取url内参数
 function queryURLParams(url) {
-    let pattern = /(\w+)=(\w+)/ig; //定义正则表达式
+    let pattern = /(\w+)=([\w-_]+)/ig; //定义正则表达式
     let params = {}; // 定义参数对象
     url.replace(pattern, ($, $1, $2) => {
         params[$1] = $2;
@@ -8,13 +8,47 @@ function queryURLParams(url) {
     return params;
 }
 
+// 插入数据
+function insertDataHtml(page, dataSource) {
+    var col1 = document.querySelector('#faxian-col1');
+    var col2 = document.querySelector('#faxian-col2');
+    col1.innerHTML = "";
+    col2.innerHTML = "";
+    var dataList = Object.keys(dataSource);
+    //动态插入数据
+    for (var i = 0; i < dataList.length; i++) {
+        var name = dataList[i];
+        if (name) {
+            var section = document.createElement('section');
+            var link = document.createElement('a');
+            var image = document.createElement('img');
+            section.id = name;
+            section.classList.add('eds-note-card');
+            section.classList.add('note-card');
+            image.dataset.src = "./images/content/" + name + "/" + name.replace('article-', '').replace('video-', '') + ".png";
+            image.style.width = "100%";
+            image.style.minHeight = "250px";
+            if (name.indexOf('video-') > -1) {
+                link.href = "./video.html?from=" + page + "&id=" + name;
+            } else {
+                link.href = "./normal.html?from=" + page + "&id=" + name;
+            }
+            link.appendChild(image);
+            section.appendChild(link);
+            if (i % 2 === 0) {
+                col1.appendChild(section);
+            } else {
+                col2.appendChild(section);
+            }
+        }
+    };
+}
+
 // 图片懒加载
 function lazyLoad(imgs) {
     var H = document.documentElement.clientHeight;//获取可视区域高度
     var S = document.documentElement.scrollTop || document.body.scrollTop;
-    console.log(`H: ${H}, S: ${S}, H+S: ${H+S}, `);
     for (var i = 0; i < imgs.length; i++) {
-        console.log(`i: ${i}, getTop(imgs[i]): ${getTop(imgs[i])}`);
         if (H + S > getTop(imgs[i])) {
             imgs[i].src = imgs[i].getAttribute('data-src');
         }
